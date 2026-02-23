2f4777857609a82ef64dd3c458e09195504959af
Piyush Saxena <piyushsaxena725@gmail.com>
2026-02-23 16:14:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.post(
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://examnotesaiclient-rekp.onrender.com",
     credentials: true,
     methods: ["GET", "PUT", "DELETE", "OPTIONS"]
   }),
